e448ffe717835733c3e379a80243eec6d730d5d1
sirisayshello <veckovis.salongen0p@icloud.com>
2024-06-08 16:25:45 +0200

    UI fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Board } from "./components/Board/Board";
 
 function App() {
   return (
-    <main className="p-4">
+    <main className="p-4 md:p-8">
       <Board />
     </main>
   );
--- src/components/WinModal/SolvedModal.jsx
+++ src/components/WinModal/SolvedModal.jsx
@@ -5,9 +5,9 @@ export const SolvedModal = forwardRef(({ close }, ref) => {
   return (
     <dialog
       ref={ref}
-      className="bg-tile-pink dark:bg-dark-pink backdrop:bg-gradient-to-r from-cyan-500 to-blue-500 p-4 rounded-md w-full max-w-[400px]"
+      className="bg-tile-pink dark:bg-dark-pink backdrop:bg-gradient-to-tr from-board-orange from-25% to-tile-pink backdrop:opacity-95 p-4 rounded-md w-full max-w-[300px] md:max-w-[400px]"
     >
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-4">
         <div>You did it!</div>
         <Button onclick={close}>Got it</Button>
       </div>
