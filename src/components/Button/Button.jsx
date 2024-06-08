26da73490a3bcbd271341861b3038536a7382f50
sirisayshello <veckovis.salongen0p@icloud.com>
2024-06-08 14:09:13 +0200

    generic Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
-export const ShuffleButton = ({ shuffle }) => {
+export const Button = ({ onclick, children }) => {
   return (
     <button
       className="bg-board-orange dark:bg-dark-orange py-2 px-6 rounded-md text-xl"
-      onClick={shuffle}
+      onClick={onclick}
     >
-      SLUMPA
+      {children}
     </button>
   );
 };
