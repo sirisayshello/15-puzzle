2793c32d61b161a3f1e986c6d31e422e042812e5
sirisayshello <veckovis.salongen0p@icloud.com>
2024-06-08 10:58:28 +0200

    update styling tile and button

--- src/components/Button/ShuffleButton.jsx
+++ src/components/Button/ShuffleButton.jsx
@@ -1,3 +1,10 @@
 export const ShuffleButton = ({ shuffle }) => {
-  return <button onClick={shuffle}>SLUMPA</button>;
+  return (
+    <button
+      className="bg-board-orange dark:bg-dark-orange py-2 px-6 rounded-md text-xl"
+      onClick={shuffle}
+    >
+      SLUMPA
+    </button>
+  );
 };
--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -1,6 +1,9 @@
-const Tile = ({ number }) => {
+const Tile = ({ number, move }) => {
   return (
-    <div className="flex justify-center items-center bg-red-400">
+    <div
+      className="flex justify-center items-center text-3xl bg-tile-pink dark:bg-dark-pink rounded hover:cursor-pointer"
+      onClick={move}
+    >
       <p>{number}</p>
     </div>
   );
