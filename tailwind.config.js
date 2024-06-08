7f99aeb70f9ab088bbcf1ff75ef4c3454c9d12f1
sirisayshello <veckovis.salongen0p@icloud.com>
2024-06-08 14:08:31 +0200

    tailwind config and update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "board-orange": "#ea8d48",
+      "tile-pink": "#FCF8FA",
+      "dark-orange": "#1A1625",
+      "dark-pink": "#2F2B3A",
+    },
   },
   plugins: [],
 };
